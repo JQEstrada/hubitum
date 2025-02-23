c244297bb9e980bff6f644959bb53f427e8dd76b
JQEstrada <joaoestrada@msn.com>
2025-02-23 01:19:34 +0000

    Prepare version 2 testing

---
+++ quasar-project/src-capacitor/ios/App/App/public/assets/AuthenticationService.cf6e7708.js
@@ -0,0 +1,1 @@
+import{ar as t}from"./index.292b926d.js";var i={register(r){return t().post("register",r)},login(r){return t().post("login",r)}};export{i as A};
---
+++ quasar-project/src-capacitor/ios/App/App/public/assets/pinia.ee1466f8.js
@@ -0,0 +1,1 @@
+import{b as t,c as s,s as e}from"./index.292b926d.js";var n=t(({app:i})=>{const a=s();e(a),i.use(a).mount("q-app")});export{n as default};
